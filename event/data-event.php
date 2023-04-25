bfa715ec4da5d0c529a3841b16d5bba38316aca0
Jacky <jackyhau97@gmail.com>
2023-04-25 12:49:28 +0700

    update event ajax done

---
+++ event/data-event.php
@@ -0,0 +1,29 @@
+<?php 
+  require_once "../config.php";
+  
+  if (isset($_POST["tipe"]))
+  {
+    $tipe = $_POST["tipe"];
+    $result = $conn->query("SELECT * FROM tb_event WHERE genre_game = '$tipe' ORDER BY id DESC");
+    while ($row = $result->fetch_array())
+    {
+      $path = BASE_URL.DS.'assets/img/event/'.$row['path'];
+      $nama = $row['nama'];
+      $data = '<div class="col-12 col-md-6 mb-4">
+                  <div class="card">
+                    <img src="'.$path.'" class="card-img-top" alt="'.$nama.'">
+                    <div class="card-body">
+                      <p class="card-text fw-bold">'.$nama.'</p>
+                    </div>
+                  </div>
+                </div>';
+    }
+  }
+  else
+  {
+    $data .= 'No Event.';
+  }  
+  $data .= '</div>';
+
+  echo $data;
+?>
--- event/index.php
+++ event/index.php
@@ -23,68 +23,137 @@
           <img src="<?=BASE_URL.DS.'assets/img/home/esports.jpg'?>" class="d-block w-100" alt="...">
         </div>
         <div class="col-12 mt-5">
-          <button type="button" class="btn btn-outline-primary rounded-pill">HOT</button>
-          <button type="button" class="btn btn-outline-primary rounded-pill">MOBA</button>
-          <button type="button" class="btn btn-outline-primary rounded-pill">FPS</button>
-          <button type="button" class="btn btn-outline-primary rounded-pill">Battle Royale</button>
-          <button type="button" class="btn btn-outline-primary rounded-pill">Fighting</button>
-          <button type="button" class="btn btn-outline-primary rounded-pill">TalkShow</button>
+          <button type="button" class="btn btn-outline-primary rounded-pill category event-active" id="hot">HOT</button>
+          <button type="button" class="btn btn-outline-primary rounded-pill category" id="moba">MOBA</button>
+          <button type="button" class="btn btn-outline-primary rounded-pill category" id="fps">FPS</button>
+          <button type="button" class="btn btn-outline-primary rounded-pill category" id="battle-royale">Battle Royale</button>
+          <button type="button" class="btn btn-outline-primary rounded-pill category" id="fighting">Fighting</button>
+          <button type="button" class="btn btn-outline-primary rounded-pill category" id="talkshow">TalkShow</button>
         </div>
       </div>
-      <div class="row mt-5">
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="<?=BASE_URL.DS.'assets/img/user.png'?>" class="card-img-top" alt="Event">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="<?=BASE_URL.DS.'assets/img/user.png'?>" class="card-img-top" alt="Event">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="<?=BASE_URL.DS.'assets/img/user.png'?>" class="card-img-top" alt="Event">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="<?=BASE_URL.DS.'assets/img/user.png'?>" class="card-img-top" alt="Event">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="<?=BASE_URL.DS.'assets/img/user.png'?>" class="card-img-top" alt="Event">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="<?=BASE_URL.DS.'assets/img/user.png'?>" class="card-img-top" alt="Event">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
+      <div class="row mt-5" id="hasil">
+        <!-- Event Inject disini -->
       </div>
     </div>
     <!-- End -->
     <?php 
       require_once '../footer.php';
     ?>
+
+    <script>
+      function loadData() {
+        let tipe = "hot";
+        $.ajax({
+          type: "post",
+          url: "data-event.php",
+          data: {tipe: tipe},
+          success: function(data) {
+            $("#hasil").html(data);
+          },
+          error: function(data) {
+            console.log('error');
+          }
+        })
+      }
+      loadData();
+
+      $("#hot").click(function() {
+        $(".category").each(function() {
+          $(this).removeClass("event-active");
+        })
+        $(this).addClass("event-active");
+        loadData();
+      })
+      $("#moba").click(function() {        
+        let tipe = "moba";
+        $(".category").each(function() {
+          $(this).removeClass("event-active");
+        })
+        $(this).addClass("event-active");
+        $.ajax({
+          type: "post",
+          url: "data-event.php",
+          data: {tipe: tipe},
+          success: function(data) {
+            $("#hasil").html(data);
+          },
+          error: function(data) {
+            console.log('error');
+          }
+        })
+      })      
+      $("#fps").click(function() {        
+        let tipe = "fps";
+        $(".category").each(function() {
+          $(this).removeClass("event-active");
+        })
+        $(this).addClass("event-active");
+        $.ajax({
+          type: "post",
+          url: "data-event.php",
+          data: {tipe: tipe},
+          success: function(data) {
+            $("#hasil").html(data);
+          },
+          error: function(data) {
+            console.log('error');
+          }
+        })
+      })
+      $("#battle-royale").click(function() {
+        let tipe = "battle-royale";
+        $(".category").each(function() {
+          $(this).removeClass("event-active");
+        })
+        $(this).addClass("event-active");
+        $.ajax({
+          type: "post",
+          url: "data-event.php",
+          data: {tipe: tipe},
+          success: function(data) {
+            $("#hasil").html(data);
+          },
+          error: function(data) {
+            console.log('error');
+          }
+        })
+      })      
+      $("#fighting").click(function() {
+        let tipe = "fighting";
+        $(".category").each(function() {
+          $(this).removeClass("event-active");
+        })
+        $(this).addClass("event-active");
+        $.ajax({
+          type: "post",
+          url: "data-event.php",
+          data: {tipe: tipe},
+          success: function(data) {
+            $("#hasil").html(data);
+          },
+          error: function(data) {
+            console.log('error');
+          }
+        })
+      })
+      $("#talkshow").click(function() {
+        let tipe = "talkshow";
+        $(".category").each(function() {
+          $(this).removeClass("event-active");
+        })
+        $(this).addClass("event-active");
+        $.ajax({
+          type: "post",
+          url: "data-event.php",
+          data: {tipe: tipe},
+          success: function(data) {
+            $("#hasil").html(data);
+          },
+          error: function(data) {
+            console.log('error');
+          }
+        })
+      })
+    </script>
   </body>
 </html>
